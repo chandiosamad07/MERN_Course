73e5004540d542735b4cb4a738a8066e4fa48100
chandiosamad07 <absamadchandio@gmamil.com>
2024-07-28 11:31:29 +0500

    JavaScript Basic Variable Data Type

---
+++ JS/Loops.js
@@ -0,0 +1,9 @@
+let counter =10;
+for(let a=1; a<=5;a++){
+    console.log(a)
+}
+
+while(counter>0){
+console.log(counter);
+counter--
+}
---
+++ JS/Test.js
@@ -0,0 +1,56 @@
+
+let a= "Abdul";
+ let b= 10;
+const pi=3.142;
+ let d= true;
+let marks =59;
+
+
+if(marks >=90){
+    console.log("A Grade")
+}
+
+else if(marks >=80){
+    console.log("B Grade")
+}
+
+
+else if(marks >=70){
+    console.log("C Grade")
+}
+
+
+else if(marks >=60){
+    console.log("D Grade")
+}
+
+else{
+    console.log('F Grade')
+}
+
+
+// console.log(typeof (a))
+// console.log(typeof (b))
+// console.log(typeof (pi))
+// console.log(typeof (d))
+
+// console.log(b>pi);
+
+// console.log(b<pi);
+
+// console.log(b>=pi);
+
+// console.log(b<=pi);
+
+
+//Conditional Statement
+
+// if(b==pi && b>pi){
+//     console.log("B is equal to pi")
+// }
+// else if (b>pi){
+//     console.log("B is Grater then pi")
+// }
+// else{
+//     console.log("pi is less than  b")
+// }
