19183f868455ecb7fd5b49e8626e7e83211b3e1a
chandiosamad07 <absamadchandio@gmamil.com>
2024-07-28 11:49:10 +0500

    Array

---
+++ JS/Array.js
@@ -0,0 +1,12 @@
+let Names = ["ibrahim","Abdullah","Abdul","Ghafoor"];
+
+Names[2]="Imran"
+// for(let a=0;a<4;a++){
+//     console.log(Names[a])
+// }
+
+let num =[1,2,3,4,5,6,7]
+
+for(let a=0;a<7;a++){
+    console.log(num[a])
+}
--- JS/Loops.js
+++ JS/Loops.js
@@ -1,9 +1,13 @@
-let counter =10;
-for(let a=1; a<=5;a++){
-    console.log(a)
-}
+// let counter =10;
+// for(let a=1; a<=5;a++){
+//     console.log(a)
+// }
 
-while(counter>0){
-console.log(counter);
-counter--
+// while(counter>0){
+// console.log(counter);
+// counter--
+// }
+console.log("Table of 5")
+for(let i=1;i<=10;i++){
+    console.log(i + " * 5"," = " ,(i*5))
 }
