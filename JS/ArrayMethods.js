6b0250e454942f06527f9d177c568590ec9126c7
chandiosamad07 <absamadchandio@gmamil.com>
2024-08-03 09:35:05 +0500

    Array Method push pop shift unshift length

---
+++ JS/ArrayMethods.js
@@ -0,0 +1,12 @@
+let array = [3,2,1]
+
+let size = array.length;
+// array.pop()
+// array.push(4)
+// array.shift();
+// array.shift();
+// array.unshift(5);
+// console.log(size)
+for(let i=0;i<size;i++){
+    console.log(array[i])
+}
