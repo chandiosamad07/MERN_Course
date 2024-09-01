9359c99df2dde26afa61ef02b7d6bab347f853fc
chandiosamad07 <absamadchandio@gmamil.com>
2024-09-01 11:13:36 +0500

    APi integration

--- React/lrean-react/src/Components/APIC.jsx
+++ React/lrean-react/src/Components/APIC.jsx
@@ -1,37 +1,43 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const APIC = () => {
-    const [posts , setPosts] =useState([])
+  const [posts, setPosts] = useState([]);
 
-    useEffect(()=>{
-
-        const FetchPost = async ()=>{
-            try{
-            let response = await fetch('http://localhost:5000/Getdata')
-            let result = await response.json()
-            console.log(result)
-            setPosts(result)
-          
-            }
-            catch{
-                console.log("Api data not find")
-            }
-        }
-        FetchPost()
-    },[])
+  useEffect(() => {
+    const FetchPost = async () => {
+      try {
+        let response = await fetch("http://localhost:5000/Getdata");
+        let result = await response.json();
+        console.log(result);
+        setPosts(result);
+      } catch {
+        console.log("Api data not find");
+      }
+    };
+    FetchPost();
+  }, []);
 
   return (
-    <div> <div>
-    {
-        posts.map((post,index)=>(
-            <div key={index}>
-                <h3>{post.name}</h3>
-                <h3>{post.Company}</h3>
-            </div>
-        ))
-    }
-</div></div>
-  )
-}
+    <div>
+      {" "}
+      <div>
+        {posts.map((post, index) => (
+          <div key={index}>
+            <table>
+              <tr>
+                <td>
+                  <h3>{post.name}</h3>
+                </td>
+                <td>
+                  <h3>{post.Company}</h3>
+                </td>
+              </tr>
+            </table>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default APIC
+export default APIC;
