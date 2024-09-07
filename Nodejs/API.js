643bb5c64e7edd990fccbb9abd368c09ce5b7104
chandiosamad07 <absamadchandio@gmamil.com>
2024-09-07 11:28:26 +0500

    CURD API is completed

--- Nodejs/API.js
+++ Nodejs/API.js
@@ -2,7 +2,7 @@ const express = require('express')
 const dbConnect = require('./mongodb')
 const cors = require('cors');
 const app = express()
-
+const mongodb = require('mongodb')
 app.use(express.json())
 app.use(cors()); 
 
@@ -10,6 +10,27 @@ app.get('/Getdata',async (req,res)=>{
     let data = await dbConnect();
     let result = await data.find({}).toArray()  
     res.send(result)
+})
+
+app.post('/post',async(req,res)=>{
+    console.log(req.body)
+    let data = await dbConnect();
+    let result = await data.insertOne(req.body);
+    res.send(result)
+})
+
+app.put('/update',async(req,res)=>{
+   let data = await dbConnect();
+   let result = await data.updateOne({name:req.body.name},{$set:req.body})
+   res.send(result)
+})
+
+
+app.delete('/:id',async (req,res)=>{
+    let data = await dbConnect();
+    let result = await data.deleteOne({_id: new mongodb.ObjectId (req.params.id)})
+    console.log(result)
+    res.send("done")
 })
 
 app.listen(5000,()=>{
